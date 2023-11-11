f94fe5eacf6d57ab3d827ae192b1779ebd127a69
afishspring <104251938+afishspring@users.noreply.github.com>
2023-11-11 16:25:07 +0800

    resnet

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from tqdm import tqdm
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from sklearn.metrics import confusion_matrix
-from utils import load_data, plot_confusion_matrix, plot_accuracy, plot_loss
-from models import cnn_classify
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from utils import *
+from models import *
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='resnet50',
+                    help='cnn/resnet34/.../resnet101')
 # 完整训练次数
-parser.add_argument('--epochs', type=int, default=50,
+parser.add_argument('--epochs', type=int, default=100,
                     help='Number of epochs to train.')
 # 学习率
-parser.add_argument('--lr', type=float, default=0.001,
+parser.add_argument('--lr', type=float, default=0.0001,
                     help='Initial learning rate.')
 # batch_size
 parser.add_argument('--batch_size', type=int, default=512,
@@ -27,70 +29,98 @@ args = parser.parse_args()
 
 train_loader, val_loader, test_loader = load_data()
 
-model = cnn_classify()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model.to(device)
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(),
-                       lr=args.lr, weight_decay=args.weight_decay)
+def train(model_name):
+    model = get_model(model_name)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(),
+                        lr=args.lr, weight_decay=args.weight_decay)
 
-
-def train():
     train_losses = []
-    train_accuracy = []
+    val_accuracies = []
+    
     for epoch in range(args.epochs):
+        model.train()
         losses = []
-        accuracy = []
         loop = tqdm((train_loader), total=len(train_loader))
         for inputs, labels in loop:
             inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
-
-            #前向 + 反向 + 更新
             outputs = model(inputs)
             loss = criterion(outputs, labels.long())
-            losses.append(loss)
             loss.backward()
             optimizer.step()
 
-            _, predictions = outputs.max(1)
-            num_correct = (predictions == labels).sum()
-            running_train_acc = float(num_correct) / float(inputs.shape[0])
-            accuracy.append(running_train_acc)
+            losses.append(loss.item())
+            loop.set_description(f'Train epoch [{epoch}/{args.epochs}]')
+            loop.set_postfix(loss = loss.item())
 
-            loop.set_description(f'Epoch [{epoch}/{args.epochs}]')
-            loop.set_postfix(loss=loss.item(), acc=running_train_acc)
-
-        epoch_loss = (sum(losses) / len(losses)).item()
-        epoch_accuracy = sum(accuracy) / len(accuracy)
-
-        train_losses.append(epoch_loss)
-        train_accuracy.append(epoch_accuracy)
+        model.eval()
+        accuracy = []
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predictions = outputs.max(1)
+                num_correct = (predictions == labels).sum()
+                running_train_acc = float(num_correct) / float(inputs.shape[0])
+                accuracy.append(running_train_acc)
+        
+        train_loss = sum(losses)/len(losses)
+        val_accurate = sum(accuracy)/len(accuracy)
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %(epoch + 1, train_loss, val_accurate))
+        
+        train_losses.append(train_loss)
+        val_accuracies.append(val_accurate)
     
-    print('finish train')
-    plot_loss(train_losses)
-    plot_accuracy(train_accuracy)
+    print('Finish train.')
+    plot_loss(model_name, train_losses)
+    plot_accuracy(model_name, val_accuracies)
 
-def test():
+    # test
     all_labels = []
     all_predictions = []
+    all_predictions_pro = []
     with torch.no_grad():
         for inputs, labels in test_loader:
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)
+            predicted_probabilities = torch.softmax(outputs, dim=1).cpu().detach().numpy()
             _, predictions = torch.max(outputs, 1)
 
             all_labels.extend(labels.cpu().numpy())
             all_predictions.extend(predictions.cpu().numpy())
+            all_predictions_pro.extend(predicted_probabilities)
+    
+    test_accuracy = accuracy_score(all_labels, all_predictions)
+    print(f'Accuracy: {test_accuracy:.3f}')
+
+    report = classification_report(all_labels, all_predictions)
+    print("Classification Report:")
+    print(report)
 
     cm = confusion_matrix(all_labels, all_predictions)
-    plot_confusion_matrix(cm)
+    plot_confusion_matrix(model_name, cm)
 
-    correct = sum(np.array(all_labels) == np.array(all_predictions))
-    test_accuracy = correct / len(all_labels)
-    print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
+    plot_auroc(model_name, all_labels, all_predictions_pro)
+
+    return train_losses, val_accuracies
 
 
-train()
-test()
+model_configs = [
+    'resnet34',
+    'resnet50', 
+    'resnet101'
+]
+all_train_losses = []
+all_val_accuracies = []
+for model_name in model_configs:
+    print(f"Training {model_name}...")
+    train_losses, val_accuracies = train(model_name)
+    all_train_losses.append(train_losses)
+    all_val_accuracies.append(val_accuracies)
+
+plot_compare_accuracy(model_configs, all_val_accuracies)
+plot_compare_loss(model_configs, all_train_losses)
--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import auc, roc_curve
 import torch
 from torch.utils.data import DataLoader, TensorDataset
-
 
 def load_data():
     fashion_train_df = pd.read_csv('data/fashion-mnist_train.csv', sep=',')
@@ -43,31 +43,80 @@ def tensor_data(X_data, y_data):
 # 绘制混淆矩阵
 
 
-def plot_confusion_matrix(cm):
+def plot_confusion_matrix(model, cm):
     plt.figure(figsize=(8, 8))
     sns.set()
     sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
     plt.ylabel('True')
+    plt.savefig('figure/confusion_matrix_'+model+'.jpg', dpi=1000)
+    plt.show()
+    
+def plot_auroc(model, all_labels, all_predictions_pro):
+    all_predictions_pro=np.vstack(all_predictions_pro)
+
+    true_labels = np.array(all_labels)
+    predicted_probabilities = np.array(all_predictions_pro)
+
+    plt.figure(figsize=(10, 8))
+    for class_idx in range(predicted_probabilities.shape[1]):
+        class_true_labels = true_labels == class_idx
+        class_predicted_probs = predicted_probabilities[:, class_idx]
+        fpr, tpr, _ = roc_curve(class_true_labels, class_predicted_probs)
+        auroc = auc(fpr, tpr)
+        
+        plt.plot(fpr, tpr, label=f'Class {class_idx} (AUROC = {auroc:.2f})')
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('AUROC Curve for Multi-Class Classification')
+    plt.legend(loc="best")
+    plt.savefig('figure/AUROC_'+model+'.jpg', dpi=1000)
     plt.show()
 
 
-def plot_accuracy(accuracy):
+def plot_accuracy(model, accuracy):
     plt.figure()
     plt.plot(accuracy, label='Accuracy')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
-    plt.title('Training Accuracy')
+    plt.title('Value Accuracy')
+    plt.savefig('figure/value_accuracy_'+model+'.jpg', dpi=1000)
     plt.show()
 
 
-def plot_loss(losses):
+def plot_loss(model, losses):
     plt.figure()
     plt.plot(losses, label='Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
     plt.title('Training Loss')
+    plt.savefig('figure/train_loss_'+model+'.jpg', dpi=1000)
+    plt.show()
+
+def plot_compare_accuracy(model_configs, all_val_accuracies):
+    plt.figure(figsize=(12, 6))
+    for i, model_name in enumerate([config for config in model_configs]):
+        plt.plot(all_val_accuracies[i], label=model_name+'_Accuracy')
+
+    plt.xlabel('Epochs')
+    plt.ylabel('Validation Accuracy')
+    plt.title('Validation Accuracy for Different Models')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_compare_loss(model_configs, all_train_losses):
+    plt.figure(figsize=(12, 6))
+    for i, model_name in enumerate([config for config in model_configs]):
+        plt.plot(all_train_losses[i], label=model_name+'_Loss')
+
+    plt.xlabel('Epochs')
+    plt.ylabel('Training Loss')
+    plt.title('Training Loss for Different Models')
+    plt.legend()
+    plt.grid(True)
     plt.show()
